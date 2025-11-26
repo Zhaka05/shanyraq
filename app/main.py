07b1ead38ac48a1649c832ab66437da0df356ee0
Zhaka <shingis.zhansen@gmail.com>
2025-11-26 17:28:50 -0600

    add login functionality and jwt tokens, fix typo in database.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Form, Depends
 from fastapi.responses import Response
 from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
+from jose import jwt
 
 # repository
 from app.user_repository import UserRepo, UserCreate, User
@@ -17,7 +18,8 @@ from sqlalchemy.orm import Session
 app = FastAPI()
 users_repo = UserRepo()
 
-oauth2 = OAuth2PasswordBearer(tokenUrl="")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/users/login")
+
 
 def get_db():
     db = SessionLocal()
@@ -26,6 +28,10 @@ def get_db():
     finally:
         db.close()
 
+def create_access_token(user_id: str) -> str:
+    to_encode = {"user_id": user_id}
+    encoded_jwt = jwt.encode(to_encode, "some-secret", "HS256")
+    return encoded_jwt
 
 class UserRequest(BaseModel):
     username: str
@@ -39,6 +45,13 @@ def post_user(user: UserRequest, db: Session = Depends(get_db)):
     users_repo.add_user(db, UserCreate(username=user.username, phone=user.phone, password=user.password, name=user.name, city=user.city))
     return Response(status_code=202)
     
+@app.post("/auth/users/login")
+def post_login_user(
+    username: str = Form(),
+    password: str = Form(),
+):
+    token = create_access_token(username)
+    return {"access_token": token}
     
 
 
